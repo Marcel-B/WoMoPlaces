7018e9e77c608a65ebb7d11274b106a73c8f9cdc
Marcel Benders <marcel.benders@icloud.com>
2020-02-09 17:17:28 +0100

    Initial Commit

---
+++ WoMoPlaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WoMoPlaces
+//
+//  Created by Marcel Benders on 09.02.20.
+//  Copyright © 2020 Marcel Benders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
